4dab46ebd6cabb25d5b0e0e15aec3a7bb3ce6dcd
Yashwanth Gondi <yashreddy.g@gmail.com>
2016-02-04 11:45:18 +0530

    added basic routes to setup, get users

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
 // =======================
 // get the packages we need ============
 // =======================
-var express     = require('express');
-var app         = express();
-var bodyParser  = require('body-parser');
-var morgan      = require('morgan');
-var mongoose    = require('mongoose');
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+var mongoose = require('mongoose');
 
-var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
+var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var config = require('./config'); // get our config file
-var User   = require('./app/models/user'); // get our mongoose model
+var User = require('./app/models/user'); // get our mongoose model
 
 // =======================
 // configuration =========
@@ -19,7 +19,9 @@ mongoose.connect(config.database); // connect to database
 app.set('superSecret', config.secret); // secret variable
 
 // use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 
 // use morgan to log requests to the console
@@ -32,9 +34,50 @@ app.use(morgan('dev'));
 app.get('/', function(req, res) {
     res.send('Hello! The API is at http://localhost:' + port + '/api');
 });
+app.get('/setup', function(req, res) {
 
+    // create a sample user
+    var nick = new User({
+        name: 'Nick Cerminara',
+        password: 'password',
+        admin: true
+    });
+
+    // save the sample user
+    nick.save(function(err) {
+        if (err) throw err;
+
+        console.log('User saved successfully');
+        res.json({
+            success: true
+        });
+    });
+});
 // API ROUTES -------------------
-// we'll get to these in a second
+
+// get an instance of the router for api routes
+var apiRoutes = express.Router();
+
+// TODO: route to authenticate a user (POST http://localhost:8080/api/authenticate)
+
+// TODO: route middleware to verify a token
+
+// route to show a random message (GET http://localhost:8080/api/)
+apiRoutes.get('/', function(req, res) {
+    res.json({
+        message: 'Welcome to the coolest API on earth!'
+    });
+});
+
+// route to return all users (GET http://localhost:8080/api/users)
+apiRoutes.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
+});
+
+// apply the routes to our application with the prefix /api
+app.use('/api', apiRoutes);
 
 // =======================
 // start the server ======
